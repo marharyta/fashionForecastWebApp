2114a9a591e6b863b5dbf72595fef3a6c6d4b635
Vas <contact@vasily.cc>
2016-07-23 20:07:28 +0300

    Set up a simple webserver with livereload

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,51 @@
-var gulp	= require('gulp');
-var sass	= require('gulp-sass');
-var compass	= require('gulp-compass');
+var gulp		= require('gulp'),
+	sass		= require('gulp-sass'),
+	compass		= require('gulp-compass'),
+	webserver	= require('gulp-webserver');
+
+var sourcePaths = {
+	styles: ['sass/**/*.sass', 'sass/**/*.scss']
+}
+var distPaths = {
+	styles: 'css'
+};
+var server = {
+	host: '0.0.0.0',
+	port: '9000'
+}
 
 gulp.task('styles', function() {
-	gulp.src('sass/**/*.sass')
+	gulp.src(sourcePaths.styles)
 		.pipe(compass({
-			config_file: './config.rb',
-			css: 'css',
-			sass: 'sass'
+			config_file:	'./config.rb',
+			css:			distPaths.styles
 		}))
 		.pipe(sass().on('error', sass.logError))
 });
 
-gulp.task('default',function() {
-	gulp.watch('sass/**/*.sass',['styles']);
+gulp.task('watch',function() {
+	gulp.watch(sourcePaths.styles,['styles']);
 });
+
+gulp.task('webserver', function() {
+	gulp.src('.')
+		.pipe(webserver({
+			livereload: {
+				enable: true,
+				filter: function(fileName){
+					if (fileName.match(/.scss$/) || fileName.match(/.sass$/)){
+						// Don't livereload when sass files are changed, the compiled .css will be enough
+						return false;
+					}
+					else{
+						return true;
+					}
+				}
+			},
+			open:		true,
+			host:		server.host,
+			port:		server.port
+		}));
+});
+
+gulp.task('serve', ['watch', 'webserver']);
--- README.md
+++ README.md
@@ -6,5 +6,8 @@ Fashion Forecast is a web app that allows people to select their outfit easily
 Run `bundle install` to pull the required Ruby gems;
 Run `npm install` to get gulp etc.
 
+## Running the app
+Run `gulp serve` to start the webserver, this will open a browser tab with the app. If you're running a headless server, navigate to the IP:PORT of your server, e.g. 192.168.1.35:9000. 
+
 ## Styles compilation
 Run `gulp styles` to compile styles once, or `gulp default` to watch changes and compile in real time.
