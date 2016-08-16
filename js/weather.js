e9c4f1bd793ade201ca7f1fb758fd6bdf1ab43e2
marharyta <marharyta.obraztsova@gmail.com>
2016-08-16 23:14:39 +0300

    older version of weather.js

--- js/weather.js
+++ js/weather.js
@@ -77,6 +77,32 @@ var weather = function(){
 	}
 
 	function calculateFeelsLike(temperature, humidity, wind){
+
+		function calculateHeatBase(){
+			  // Steadman's result
+	        var heatIndex = 0.5 * (t + 61 + (t - 68) * 1.2 + h * 0.094);
+
+	        // regression equation of Rothfusz is appropriate
+	        if (t >= 80) {
+	            var heatIndexBase = (-42.379                      +
+	                                   2.04901523 * t             +
+	                                  10.14333127         * h     +
+	                                  -0.22475541 * t     * h     +
+	                                  -0.00683783 * t * t         +
+	                                  -0.05481717         * h * h +
+	                                   0.00122874 * t * t * h     +
+	                                   0.00085282 * t     * h * h +
+	                                  -0.00000199 * t * t * h * h);
+	            // adjustment
+	            if (h < 13 && t <= 112) {
+	                heatIndex = heatIndexBase - (13 - h) / 4 * Math.sqrt((17 - Math.abs(t - 95)) / 17);
+	            } else if (h > 85 && t <= 87) {
+	                heatIndex = heatIndexBase + ((h - 85) / 10) * ((87 - t) / 5)
+	            } else {
+	                heatIndex = heatIndexBase;
+	            }
+	        }
+		}
 		//calculate heat index
 		function heatIndex(temperature, humidity, wind){
 			if(convertToFahrenheitFromKelvin(temperature) >= 80 && humidity >= 40){
